7c459c92f56e66acc92f15cfd1bb060b06f7877e
CodyMacc <unconfigured@null.spigotmc.org>
2026-07-16 09:08:41 -0700

    Final Challenge Completed.

--- FCC-DecisionLogic/Program.cs
+++ FCC-DecisionLogic/Program.cs
@@ -1,4 +1,6 @@
-﻿Random dice = new Random();
+﻿using System.Xml;
+
+Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
@@ -51,3 +53,33 @@ if(message.Contains("fox"))
     Console.WriteLine("What does the fox say?");
 }
 
+
+//Challenge business rules
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration <= 10 && daysUntilExpiration > 5)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else if (daysUntilExpiration <= 5 && daysUntilExpiration > 1)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine("Your subscription expires within a day!");
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else
+{
+
+}
